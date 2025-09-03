17e953346ec0c4af80b3b406e73db5097fd0ee1c
Jhonatan Usuga Florez <aprendiz.proyectos@sao6.local>
2025-09-03 15:55:34 -0500

    Fix: Corrección en el archivo base.html por nombre incorrecto

--- admintaskG/settings.py
+++ admintaskG/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY'),
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.90.33:3015", "192.168.90.33"]
+ALLOWED_HOSTS = ["192.168.90.33:3015", "192.168.90.33", "localhost", "127.0.0.1"]
 
 
 # Application definition
