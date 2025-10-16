16750f1c79358445b10d22c7bbaa6c21ac0e3a73
Jhonatanhub <jhonatanusuga.10@gmail.com>
2025-10-16 11:14:33 -0500

    fix: Agregando los allowedhost

--- admintaskG/settings.py
+++ admintaskG/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ["192.168.90.33:3015", "192.168.90.33", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.90.33:3015", "192.168.90.33", "localhost", "127.0.0.1", "gestion-tareas.sao6.com.co", "www.gestion-tareas.sao6.com.co"]
 
 
 # Application definition
